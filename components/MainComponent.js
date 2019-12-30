bb0cc43da23fb266d2d91375ee5df6a692c9c21f
Tami Lankford <tklankford@gmail.com>
2019-12-30 14:59:28 -0600

    Components Part 2 - CampsiteIngo

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
 import { CAMPSITES } from '../shared/campsites';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES
+            campsites: CAMPSITES,
+            selectedCampsite: null
         };
     }
 
+    onCampsiteSelect(campsiteId) {
+        this.setState({selectedCampsite: campsiteId});
+    }
+
     render() {
-        return <Directory campsites={this.state.campsites} />;
+        return (
+            <View style={{flex: 1}}>
+                <Directory campsites={this.state.campsites} onPress={campsiteId => this.onCampsiteSelect(campsiteId)} />
+                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+            </View>
+        )
+
+        
     }
 }
 
